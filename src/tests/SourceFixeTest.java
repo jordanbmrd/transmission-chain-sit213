57956232eae5cd63262f24205cdc83084a93027a
Maxime <maxime.pere@imt-atlantique.net>
2024-09-16 11:58:14 +0200

    SourceFixeTest update

--- src/tests/SourceFixeTest.java
+++ src/tests/SourceFixeTest.java
@@ -5,7 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import sources.SourceFixe;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class SourceFixeTest {
 
@@ -13,10 +13,13 @@ public class SourceFixeTest {
 
     @Before
     public void setUp() {
-        String messageString = "10101"; // Example test message
+        String messageString = "10101"; // Test message
         sourceFixe = new SourceFixe(messageString);
     }
 
+    /**
+     * Test the constructor of SourceFixe
+     */
     @Test
     public void testConstructor() {
         // Ensure the information is generated correctly from the message string
@@ -27,47 +30,75 @@ public class SourceFixeTest {
         expectedInformation.add(false); // '0' -> false
         expectedInformation.add(true);  // '1' -> true
 
-        assertEquals("The generated information should match the expected information",
-                expectedInformation, sourceFixe.informationGeneree);
+        assertEquals("The generated information should match the expected information", expectedInformation, sourceFixe.informationGeneree);
     }
 
+    /**
+     * Test the informationEmise method of SourceFixe
+     */
     @Test
     public void testInformationEmise() {
         // The informationEmise should be the same as informationGeneree
-        assertEquals("The emitted information should be the same as the generated information",
-                sourceFixe.informationGeneree, sourceFixe.getInformationEmise());
+        assertEquals("The emitted information should be the same as the generated information", sourceFixe.informationGeneree, sourceFixe.getInformationEmise());
     }
 
+    /**
+     * Test the informationGeneree method of SourceFixe
+     */
     @Test
     public void testMessageStringWithOnlyOnes() {
         // Test case where the messageString is all '1's
-        String allOnes = "11111";
-        sourceFixe = new SourceFixe(allOnes);
+        String ones = "11111";
+        sourceFixe = new SourceFixe(ones);
 
         Information<Boolean> expectedInformation = new Information<>();
-        for (int i = 0; i < allOnes.length(); ++i) {
+
+        for (int i = 0; i < ones.length(); ++i) {
             expectedInformation.add(true); // '1' -> true
         }
 
-        assertEquals("The generated information should be all true for '1's",
-                expectedInformation, sourceFixe.informationGeneree);
+        assertEquals("The generated information should be all true for '1's", expectedInformation, sourceFixe.informationGeneree);
     }
 
+    /**
+     * Test the informationGeneree method of SourceFixe
+     */
     @Test
     public void testMessageStringWithOnlyZeros() {
         // Test case where the messageString is all '0's
-        String allZeros = "00000";
-        sourceFixe = new SourceFixe(allZeros);
+        String zeros = "00000";
+        sourceFixe = new SourceFixe(zeros);
 
         Information<Boolean> expectedInformation = new Information<>();
-        for (int i = 0; i < allZeros.length(); ++i) {
+
+        for (int i = 0; i < zeros.length(); ++i) {
             expectedInformation.add(false); // '0' -> false
         }
 
-        assertEquals("The generated information should be all false for '0's",
-                expectedInformation, sourceFixe.informationGeneree);
+        assertEquals("The generated information should be all false for '0's", expectedInformation, sourceFixe.informationGeneree);
     }
 
+    /**
+     * Test the informationGeneree method of SourceFixe
+     */
+    @Test
+    public void testMessageWithAlternatingBits() {
+        // Test case with alternating '1's and '0's
+        String alternatingBits = "101010";
+        sourceFixe = new SourceFixe(alternatingBits);
+
+        Information<Boolean> expectedInformation = new Information<>();
+
+        for (char bit : alternatingBits.toCharArray()) {
+            expectedInformation.add(bit == '1'); // '1' -> true, '0' -> false
+        }
+
+        assertEquals("The generated information should match the alternating bits", expectedInformation, sourceFixe.informationGeneree);
+    }
+
+    /**
+     * Test the informationGeneree method of SourceFixe
+     */
     @Test
     public void testEmptyMessageString() {
         // Test case with an empty message string
@@ -76,7 +107,6 @@ public class SourceFixeTest {
 
         Information<Boolean> expectedInformation = new Information<>();
 
-        assertEquals("The generated information should be empty for an empty message string",
-                expectedInformation, sourceFixe.informationGeneree);
+        assertEquals("The generated information should be empty for an empty message string", expectedInformation, sourceFixe.informationGeneree);
     }
 }
