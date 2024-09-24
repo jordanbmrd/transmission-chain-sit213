e5783b93b19033f7e06c642bbcc6e1648cf3accd
Maxime <maxime.pere@imt-atlantique.net>
2024-09-24 21:06:29 +0200

    Update TebTest.java

--- src/tests/TebTest.java
+++ src/tests/TebTest.java
@@ -3,28 +3,44 @@ package tests;
 import org.junit.Test;
 import simulateur.Simulateur;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 
 public class TebTest {
 
-    @Test
-    public void testSimulationMultipleTimes() throws Exception {
-        List<Float> tebList = new ArrayList<>();
-        String[] params = {"-mess", "20000", "-form", "NRZT", "-nbEch", "9", "-snrpb", "5", "-ampl", "-5", "5"};
+    private Map<Integer, Double> runSimulation(String modulation) throws Exception {
+        Map<Integer, Double> snrToTebMap = new LinkedHashMap<>(); // la HashMap classique fou le bordel dans l'affichage
+        int snr = 25;
+        double teb = 0.00;
 
-        for (int i = 0; i < 10; i++) { // Adjust the number of iterations as needed
+        while (teb <= 0.01) {
+            String[] params = {"-mess", "20000", "-form", modulation, "-nbEch", "9", "-snrpb", String.valueOf(snr)};
             Simulateur simulateur = new Simulateur(params);
-            simulateur.execute(); // Assuming execute() method exists in Simulateur class
-            float teb = simulateur.calculTauxErreurBinaire(); // Assuming getTeb() method exists in Simulateur class
-            tebList.add(teb);
+            simulateur.execute();
+            teb = simulateur.calculTauxErreurBinaire();
+            snrToTebMap.put(snr, teb);
+            snr--;
         }
 
-        System.out.println("TEB list: " + tebList);
+        return snrToTebMap;
+    }
 
-        // Validate that the list is not empty
-        assertFalse("The TEB list should not be empty", tebList.isEmpty());
+    @Test
+    public void testSimulationMultipleTimes() throws Exception {
+        Map<Integer, Double> snrToTebMapNRZT = runSimulation("NRZT");
+        System.out.println("NRZT = SNR to TEB map: " + snrToTebMapNRZT);
+
+        Map<Integer, Double> snrToTebMapNRZ = runSimulation("NRZ");
+        System.out.println("NRZ = SNR to TEB map: " + snrToTebMapNRZ);
+
+        Map<Integer, Double> snrToTebMapRZ = runSimulation("RZ");
+        System.out.println("RZ = SNR to TEB map: " + snrToTebMapRZ);
+
+        // Validate that the maps are not empty
+        assertFalse("The SNR to TEB map for NRZT should not be empty", snrToTebMapNRZT.isEmpty());
+        assertFalse("The SNR to TEB map for NRZ should not be empty", snrToTebMapNRZ.isEmpty());
+        assertFalse("The SNR to TEB map for RZ should not be empty", snrToTebMapRZ.isEmpty());
     }
 }
