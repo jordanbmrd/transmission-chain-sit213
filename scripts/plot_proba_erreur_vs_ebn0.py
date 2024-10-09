6565b47cadbd021ebe349a243d9ad3cde3071120
Pol Guillou <polguillou2@gmail.com>
2024-10-09 18:28:06 +0200

    fix: teb / Eb/N0 en théorie ; en attente de clean

--- scripts/plot_proba_erreur_vs_ebn0.py
+++ scripts/plot_proba_erreur_vs_ebn0.py
@@ -7,22 +7,21 @@ data = pd.read_csv(file_path)
 
 # Extraire les colonnes Eb/N0 et P_e
 eb_n0 = data['Eb/N0 (dB)']
-eb_n0_lin = 10**(eb_n0/10)
 pe_nrz = data['P_e NRZ']
 
-# Tracer la probabilité d'erreur en fonction de Eb/N0
+# Tracer la probabilité d'erreur en fonction de Eb/N0 en dB
 plt.figure(figsize=(10, 6))
 
-plt.plot(eb_n0_lin, pe_nrz, label="P_e NRZ", marker='s')
+plt.plot(eb_n0, pe_nrz, label="P_e NRZ", marker='s')
 
 # Ajouter les étiquettes et le titre
-plt.xlabel("SNR par bit (linéaire)")
+plt.xlabel("SNR par bit (dB)")
 plt.ylabel("TEB")
 plt.yscale('log')  # Utiliser une échelle logarithmique pour une meilleure visualisation
 plt.grid(True)
 plt.legend()
 
-plt.xlim(0, 5)
+plt.xlim(-10, 10)
 plt.ylim(10**-5, 10**-1)
 
 # Afficher le graphe
